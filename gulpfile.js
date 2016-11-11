0846447c7dea0abee02b4df76ae8241524387dea
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2016-11-10 21:07:02 -0500

    Added namespace

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,41 @@
 var gulp = require('gulp');
 var path = require('path');
-var debug = require('gulp-debug');
 var inject = require('gulp-inject-string');
 var connect = require('gulp-connect');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var fs = require('fs');
+var del = require('del');
 
-gulp.task('default', ['create:all']);
+gulp.task('default', ['clean:all','generate:all', 'create:all']);
 
-gulp.task('create:all', function () {
+gulp.task('clean:all', function () {
+  del(['tmp/*','dist/*']);
+});
+
+gulp.task('generate:all', function() {
+  // TODO find a better way of chaining this task to 'create:all'
   var files = JSON.parse(fs.readFileSync('./sources.json', 'utf-8'));
   files.forEach(function(file){
     gulp.src(file)
-      .pipe(inject.prepend('obibaCountryCodes.' + path.basename(file) + ' = '))
+      .pipe(inject.prepend('OBiBa.CountryCodes.' + path.basename(file) + ' = '))
       .pipe(inject.append(';'))
-      .pipe(gulp.dest('tmp'))
-    ;
+      .pipe(gulp.dest('tmp'));
   });
 
-  gulp.src('./tmp/*')
-    .pipe(concat('all.js'))
-    .pipe(inject.prepend('var obibaCountryCodes = {};'))
-    .pipe(gulp.dest('dist'))
-    .pipe(uglify())
-    .pipe(rename('all.min.js'))
-    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('create:all', function () {
+  // HACK to make sure all files are generated
+  setTimeout(function() {
+    gulp.src('./tmp/*')
+      .pipe(concat('all.js'))
+      .pipe(inject.prepend('var OBiBa ={}; OBiBa.CountryCodes = {};'))
+      .pipe(gulp.dest('dist'))
+      .pipe(uglify())
+      .pipe(rename('all.min.js'))
+      .pipe(gulp.dest('dist'));
+  }, 1000);
 
 });
